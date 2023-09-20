81100cd7b032e448e8085aab8923862780d784d1
rithik005 <144883829+rithik005@users.noreply.github.com>
2023-09-20 12:02:10 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,30 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+        else:
+            print("Insufficient balance.")
+
+    def display_balance(self):
+        print(f"Account Balance for {self.__account_holder_name}: {self.__account_balance}")
+
+# Creating an instance of BankAccount
+account = BankAccount("123456789", "Nithik", 5000)
+
+# Testing deposit and withdrawal functionality
+account.deposit(2000)
+account.display_balance()  # Output: Account Balance for Nithik: 7000
+
+account.withdraw(3000)
+account.display_balance()  # Output: Account Balance for Nithik: 4000
+
+account.withdraw(5000)  # Output: Insufficient balance.
+account.display_balance()  # Output: Account Balance for Nithik: 4000
